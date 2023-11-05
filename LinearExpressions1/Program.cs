827e3247f4c5398b3dd1e2bc852de9c9c6869c9e
FearlessAtom <atomgotaggressive@gmail.com>
2023-11-05 23:08:11 +0200

    commiting program files

---
+++ LinearExpressions1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq.Expressions;
+
+namespace LinearExpressions1
+{
+    internal class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.Write("What is the value of a? : ");
+            string aundone = Console.ReadLine();
+            Console.Write("What is the value of b? : ");
+            string bundone = Console.ReadLine();
+            if(bundone == "0") { Console.WriteLine("b cannot be equal to 0");return 0; }
+            Console.Write("What is the value of c? : ");
+            string cundone = Console.ReadLine();
+            if (cundone == "0") { Console.WriteLine("c cannot be equal to 0"); return 0; }
+            Console.Write("What is the value of d? : ");
+            string dundone = Console.ReadLine();
+            if (dundone == "0") { Console.WriteLine("d cannot be equal to 0"); return 0; }
+            double a = Convert.ToDouble(aundone);
+            double b = Convert.ToDouble(bundone);
+            double c = Convert.ToDouble(cundone);
+            double d = Convert.ToDouble(dundone);
+            double x = (a + 2 * b - c + d) / (c * d) + (a + b) / (c - d) - (Math.Pow(a, 2)) / (Math.Pow(b, 2));
+            string formattedx = x.ToString("0.00");
+            Console.WriteLine($"z is equal to {formattedx}");
+            double y = 5 * (a + b) * (c - d) / (1.0 / 2 * c) + Math.Pow(d, 2) * (Math.Pow(a, 2) - Math.Pow(b, 2)) / (b - a);
+            string formattedy = y.ToString("0.00");
+            Console.WriteLine($"y is equal to {formattedy}");
+            double z = ((Math.Pow(Math.Pow(x, 2) - 2 * x,3) - 4 * (Math.Pow(x,4) + 1)) * (1 - b))/(5 * a + 3 * b);
+            string formattedz = z.ToString("0.00");
+            Console.WriteLine($"z is equal to {formattedz}");
+            double r = (1 / 2 * a + 3 / 4 * b - 7 / 5) / (3 * c + 1) + 1 / (a - c);
+            string formattedr = r.ToString("0.00");
+            Console.WriteLine($"r is equal to {formattedr}");
+            return 0;
+        }
+    }
+}
---
+++ LinearExpressions2/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace LinearExpressions2
+{
+    internal class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.Write("What is the value of m ? : ");
+            string mundone = Console.ReadLine();
+            if(mundone == "0") { Console.WriteLine("m cannot be equal to 0!");return 0; }
+            double m = Convert.ToDouble(mundone);
+            Console.Write("What is the value of n ? : ");
+            string nundone = Console.ReadLine();
+            double n = Convert.ToDouble(mundone);
+            Console.Write("What is the value of x? : ");
+            string xundone = Console.ReadLine();
+            double x = Convert.ToDouble(xundone);
+            Console.Write("What is the value of a? : ");
+            string aundone = Console.ReadLine();
+            if(aundone == "0") { Console.WriteLine("a cannot be equal to 0!");return 0; }
+            double a = Convert.ToDouble(xundone);
+            Console.Write("What is the value of b? : ");
+            string bundone = Console.ReadLine();
+            double b = Convert.ToDouble(xundone);
+            double z1 = ((m - 1) * Math.Pow(m, 1.0 / 2) - (n - 1) * Math.Pow(n, 1.0 / 2)) / (Math.Pow(Math.Pow(m, 3) * n, 1.0 / 2) + n * m + Math.Pow(m, 2) - m);
+            string formattedz1 = z1.ToString("0.00");
+            Console.WriteLine($"z1 is equal to {z1}");
+            double z2 = (Math.Pow(m,1.0/2)  - Math.Pow(n,1.0/2) / (m));
+            string formattedz2 = z2.ToString("0.00");
+            Console.WriteLine($"z2 is equal to {z2}");
+            double y = 2.4 * (Math.Abs(Math.Pow(x, 2) + b) / a) + (a - b) * Math.Pow(Math.Sin(a - b), 2) + Math.Pow(10, -2) * (x - b);
+            string formattedy = y.ToString("0.00");
+            Console.WriteLine($"y is equal to {formattedy}");
+            return 0;
+        
+        }
+    }
+}
